2fd8fc71b7719539a74ae88b70e7b63362e37490
clownlessmode <clownessmode@bk.ru>
2025-09-16 11:37:47 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/foodcord-terminal",
   images: {
     minimumCacheTTL: 60 * 60 * 24,
     remotePatterns: [
