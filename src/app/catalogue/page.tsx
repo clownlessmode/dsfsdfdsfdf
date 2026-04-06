2904d1677423114b62f77cae9fdcbf23033f8ca5
Ilya <97036370+VILZAR@users.noreply.github.com>
2026-04-06 12:03:29 +0700

    Refactor category and product fetching logic to use credentials for API requests instead of cookie headers, improving security and simplifying the code. Enhance error handling by throwing errors on API failures for better debugging.

--- src/app/catalogue/page.tsx
+++ src/app/catalogue/page.tsx
@@ -12,27 +12,13 @@ async function getCategories(): Promise<ICategory[]> {
   if (!idStore) {
     return [];
   }
-
-  const cookieHeader = cookieStore
-    .getAll()
-    .map((c) => `${c.name}=${c.value}`)
-    .join("; ");
-
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/groups/get-all-group-per-store/${idStore}`,
-    {
-      headers: {
-        Cookie: cookieHeader,
-      },
-      next: { revalidate: revalidate, tags: ["catalogue", "categories"] },
-    },
-  );
-
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/groups/get-all-group-per-store/${idStore}`, {
+    credentials: "include",
+    next: { revalidate: revalidate, tags: ["catalogue", "categories"] },
+  });
   if (!res.ok) {
-    console.error(`API вернул ошибку: ${res.status} ${res.statusText}`);
-    return [];
+    throw new Error(`Ошибка API: ${res.status}`);
   }
-
   const json = (await res.json()) as ICategoryResponse;
   return json?.data ?? [];
 }
@@ -43,31 +29,19 @@ async function getProducts(): Promise<IProduct[]> {
   if (!idStore) {
     return [];
   }
-
-  const cookieHeader = cookieStore
-    .getAll()
-    .map((c) => `${c.name}=${c.value}`)
-    .join("; ");
-
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/product-main/find-all-product-per-store/${idStore}`,
-    {
-      headers: {
-        Cookie: cookieHeader,
-      },
-      next: { revalidate: revalidate, tags: ["catalogue", "products"] },
-    },
-  );
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/product-main/find-all-product-per-store/${idStore}`, {
+    credentials: "include",
+    next: { revalidate: revalidate, tags: ["catalogue", "products"] },
+  });
 
   if (!res.ok) {
-    console.error(`API вернул ошибку: ${res.status} ${res.statusText}`);
-    return [];
+    throw new Error(`Ошибка API: ${res.status}`);
   }
 
   const json = (await res.json()) as unknown as
     | IProduct[]
     | { data?: IProduct[] };
-  return Array.isArray(json) ? json : (json?.data ?? []);
+  return Array.isArray(json) ? json : json?.data ?? [];
 }
 
 const CataloguePage = async () => {
